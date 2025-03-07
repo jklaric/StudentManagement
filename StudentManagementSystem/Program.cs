9be4021a33b7e29eaa2156ff1482357aed655561
Jklaric <klaric9jakov@gmail.com>
2025-03-07 01:19:06 +0100

    Added date of birth to student

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -6,6 +6,7 @@ public class Student
     public String FirstName { get; set; }
     public String MiddleName { get; set; }
     public String LastName { get; set; }
+    public DateTime DateOfBirth { get; set; }
     public String Email { get; set; }
     public DateTime EnrollmentDate { get; set; }
 }

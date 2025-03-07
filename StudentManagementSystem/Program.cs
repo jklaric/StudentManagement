e40358a0e6277ba25477c24575498a1f0bb01739
Jklaric <klaric9jakov@gmail.com>
2025-03-07 01:16:43 +0100

    Added middle name to student

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -4,6 +4,7 @@ public class Student
 {
     public int Id { get; set; }
     public String FirstName { get; set; }
+    public String MiddleName { get; set; }
     public String LastName { get; set; }
     public String Email { get; set; }
     public DateTime EnrollmentDate { get; set; }

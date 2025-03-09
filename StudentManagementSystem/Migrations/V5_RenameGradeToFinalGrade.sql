0c45976a14a4677672bf39027f89e643f9ed48fa
Jklaric <klaric9jakov@gmail.com>
2025-03-09 16:41:55 +0100

    Generated SQL Artifact

---
+++ StudentManagementSystem/Migrations/V5_RenameGradeToFinalGrade.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+EXEC sp_rename N'[Enrollments].[Grade]', N'FinalGrade', 'COLUMN';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307003131_RenameGradeToFinalGrade', N'9.0.2');
+
+COMMIT;
+GO
+

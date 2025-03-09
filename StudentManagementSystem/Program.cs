fdf03855ec5d5480b3e03eda2033ae1d50ae27e5
Jklaric <klaric9jakov@gmail.com>
2025-03-09 18:55:49 +0100

    Created Migration and SQL artifact

--- StudentManagementSystem/Program.cs
+++ StudentManagementSystem/Program.cs
@@ -26,7 +26,7 @@ public class Enrollment
     public int Id { get; set; }
     public int StudentId { get; set; }
     public int CourseId { get; set; }
-    public int Grade { get; set; }
+    public int FinalGrade { get; set; }
     
     public Student Student { get; set; }
     public Course Course { get; set; }

4cba8298abd972793687f3fb7e9f67017f4b24a9
Jklaric <klaric9jakov@gmail.com>
2025-03-09 16:32:36 +0100

    Created SQL Artifact

---
+++ StudentManagementSystem/Migrations/V2_AddMiddleNameToStudent.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Students] ADD [MiddleName] nvarchar(max) NOT NULL DEFAULT N'';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307001544_AddMiddleNameToStudent', N'9.0.2');
+
+COMMIT;
+GO
+

3344f1f3330fc295158588acd6fbfb5aa539f190
Jklaric <klaric9jakov@gmail.com>
2025-03-09 16:36:07 +0100

    Created SQL Artifact

---
+++ StudentManagementSystem/Migrations/V3_AddDateOfBirthToStudent.sql
@@ -0,0 +1,9 @@
+﻿BEGIN TRANSACTION;
+ALTER TABLE [Students] ADD [DateOfBirth] datetime2 NOT NULL DEFAULT '0001-01-01T00:00:00.0000000';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307001823_AddDateOfBirthToStudent', N'9.0.2');
+
+COMMIT;
+GO
+

cc0294ae5904cd9fd71bc242e73815d54b586887
Jklaric <klaric9jakov@gmail.com>
2025-03-09 16:46:49 +0100

    Created SQL Artifact

---
+++ StudentManagementSystem/Migrations/V7_ModifyCourseCredits.sql
@@ -0,0 +1,15 @@
+﻿BEGIN TRANSACTION;
+DECLARE @var sysname;
+SELECT @var = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Courses]') AND [c].[name] = N'Credits');
+IF @var IS NOT NULL EXEC(N'ALTER TABLE [Courses] DROP CONSTRAINT [' + @var + '];');
+ALTER TABLE [Courses] ALTER COLUMN [Credits] decimal(18,2) NOT NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250307004303_ModifyCourseCredits', N'9.0.2');
+
+COMMIT;
+GO
+

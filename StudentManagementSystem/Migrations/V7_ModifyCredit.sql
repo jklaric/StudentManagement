0106e47591a15660cda9b9ccdc6ca41f9943d5ec
Jklaric <klaric9jakov@gmail.com>
2025-03-09 19:00:56 +0100

    Created migration and sql artifact

---
+++ StudentManagementSystem/Migrations/V7_ModifyCredit.sql
@@ -0,0 +1,116 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Courses] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] nvarchar(max) NOT NULL,
+    [Credits] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Courses] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Students] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NOT NULL,
+    [Email] nvarchar(max) NOT NULL,
+    [EnrollmentDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
+);
+
+CREATE TABLE [Enrollments] (
+    [Id] int NOT NULL IDENTITY,
+    [StudentId] int NOT NULL,
+    [CourseId] int NOT NULL,
+    [Grade] int NOT NULL,
+    CONSTRAINT [PK_Enrollments] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Enrollments_Courses_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Courses] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Enrollments_Students_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Students] ([Id]) ON DELETE CASCADE
+);
+
+CREATE INDEX [IX_Enrollments_CourseId] ON [Enrollments] ([CourseId]);
+
+CREATE INDEX [IX_Enrollments_StudentId] ON [Enrollments] ([StudentId]);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250309155426_InitialSchema', N'9.0.2');
+
+ALTER TABLE [Students] ADD [DateOfBirth] datetime2 NOT NULL DEFAULT '0001-01-01T00:00:00.0000000';
+
+ALTER TABLE [Students] ADD [MiddleName] nvarchar(max) NOT NULL DEFAULT N'';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250309155846_AddMiddleNameToStudent', N'9.0.2');
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250309160355_AddDateOfBirthToStudent', N'9.0.2');
+
+ALTER TABLE [Courses] ADD [InstructorId] int NOT NULL DEFAULT 0;
+
+CREATE TABLE [Instructor] (
+    [Id] int NOT NULL IDENTITY,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NOT NULL,
+    [Email] nvarchar(max) NOT NULL,
+    [HireDate] datetime2 NOT NULL,
+    CONSTRAINT [PK_Instructor] PRIMARY KEY ([Id])
+);
+
+CREATE INDEX [IX_Courses_InstructorId] ON [Courses] ([InstructorId]);
+
+ALTER TABLE [Courses] ADD CONSTRAINT [FK_Courses_Instructor_InstructorId] FOREIGN KEY ([InstructorId]) REFERENCES [Instructor] ([Id]) ON DELETE CASCADE;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250309175150_AddInstructorRelation', N'9.0.2');
+
+EXEC sp_rename N'[Enrollments].[Grade]', N'FinalGrade', 'COLUMN';
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250309175445_RenameGradeToFinalGrade', N'9.0.2');
+
+ALTER TABLE [Courses] DROP CONSTRAINT [FK_Courses_Instructor_InstructorId];
+
+ALTER TABLE [Instructor] DROP CONSTRAINT [PK_Instructor];
+
+EXEC sp_rename N'[Instructor]', N'Instructors', 'OBJECT';
+
+ALTER TABLE [Instructors] ADD CONSTRAINT [PK_Instructors] PRIMARY KEY ([Id]);
+
+CREATE TABLE [Departments] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Budget] decimal(18,2) NOT NULL,
+    [StartDate] datetime2 NOT NULL,
+    [DepartmentHeadId] int NULL,
+    CONSTRAINT [PK_Departments] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Departments_Instructors_DepartmentHeadId] FOREIGN KEY ([DepartmentHeadId]) REFERENCES [Instructors] ([Id]) ON DELETE SET NULL
+);
+
+CREATE INDEX [IX_Departments_DepartmentHeadId] ON [Departments] ([DepartmentHeadId]);
+
+ALTER TABLE [Courses] ADD CONSTRAINT [FK_Courses_Instructors_InstructorId] FOREIGN KEY ([InstructorId]) REFERENCES [Instructors] ([Id]) ON DELETE CASCADE;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250309175701_AddDepartmentRelation', N'9.0.2');
+
+DECLARE @var sysname;
+SELECT @var = [d].[name]
+FROM [sys].[default_constraints] [d]
+INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+WHERE ([d].[parent_object_id] = OBJECT_ID(N'[Courses]') AND [c].[name] = N'Credits');
+IF @var IS NOT NULL EXEC(N'ALTER TABLE [Courses] DROP CONSTRAINT [' + @var + '];');
+ALTER TABLE [Courses] ALTER COLUMN [Credits] decimal(18,2) NOT NULL;
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20250309175919_ModifyCredit', N'9.0.2');
+
+COMMIT;
+GO
+
